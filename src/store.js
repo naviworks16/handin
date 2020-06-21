f4ff270c99398eb8e62bc068569ff2ad4dc488eb
naviworks16 <naviworks16@gmail.com>
2020-06-22 01:32:51 +0900

    i tried

---
+++ src/store.js
@@ -0,0 +1,74 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    user: { id: 'abc123', name: 'Adam Jahr' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: [],
+    eventTotal: 0,
+    event: {}
+  },
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENT(state, event) {
+      state.event = event
+    },
+    SET_EVENT_TOTAL(state, eventTotal){
+      state.eventTotal = eventTotal
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then(response => {
+          commit('SET_EVENT_TOTAL', response.headers['x-total-count'])
+          commit('SET_EVENTS', response.data)
+        })
+        .catch(error => {
+          console.log('There was an error:', error.response)
+        })
+    },
+    fetchEvent({ commit, getters }, id) {
+      var event = getters.getEventById(id)
+
+      if (event) {
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(id)
+          .then(response => {
+            commit('SET_EVENT', response.data)
+          })
+          .catch(error => {
+            console.log('There was an error:', error.response)
+          })
+        }
+      }
+    },
+    getters: {
+      getEventById: state => id => {
+        return state.events.find(event => event.id === id)
+      }
+    }
+})
